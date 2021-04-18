35cc3a4297520bd599e8cd559bcec523dba253fe
Швырёв Станислав <ctac00025@gmail.com>
2021-04-18 14:21:04 +0300

    Initialize

---
+++ N6/TestHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace N6
+{
+	public class TestHub : Hub
+	{
+		public async Task Send(string message, string userName)
+		{
+			await Clients.All.SendAsync("Send", message, userName);
+		}
+	}
+}
